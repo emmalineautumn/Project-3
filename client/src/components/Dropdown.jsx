d8cfa56d20fad24dbbc05a8d956a6d6fb7c9928c
Mason Short <ironicallyshort737@hotmail.com>
2019-10-05 02:37:04 -0600

    fixed dropdown messing with flow

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -14,16 +14,20 @@ class Dropdown extends Component {
         return (
             <>
                 <li className="left-align" onMouseEnter={this.toggleDropdown} onMouseLeave={this.toggleDropdown}><a href={this.props.destination || '#'}>{this.props.label}</a>
-                    <ul id={this.props.id} className={"dropdown-content " + this.props.size}
-                    style={{
-                        position: 'relative',
-                        bottom: '-100%',
-                        display: this.state.displayDropdown ? 'flex' : 'none',
-                        flexDirection: 'column',
-                    }}
-                    >
-                        {this.props.children}
-                    </ul>
+                    <div style={{
+                        position: 'fixed'
+                    }}>
+                        <ul id={this.props.id} className={"dropdown-content " + this.props.size}
+                            style={{
+                                position: 'relative',
+                                bottom: '-100%',
+                                display: this.state.displayDropdown ? 'flex' : 'none',
+                                flexDirection: 'column',
+                            }}
+                        >
+                            {this.props.children}
+                        </ul>
+                    </div>
                 </li>
             </>
         );
