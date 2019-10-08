164cf9f1ac59000e5ef93a0c4ef70a15c9d2168e
Mason Short <ironicallyshort737@hotmail.com>
2019-10-08 17:08:34 -0600

    changing structure of the pagify component

--- client/src/pages/CampaignCreator.jsx
+++ client/src/pages/CampaignCreator.jsx
@@ -5,26 +5,26 @@ class CreateCampaign extends Component {
     render(){
         return (
             <div className="container grey clearfix">
-            <Pager paginate={true}>
-                <div>
-                    Hello
-                </div>
-                <div>
-                    goodbye
-                </div>
-                <div>
-                    hey
-                </div>
-                <div>
-                    see ya
-                </div>
-                <div>
-                    guten tag
-                </div>
-                <div>
-                    tchuess
-                </div>
-            </Pager>
+                <Pager paginate={true}>
+                    <div>
+                        Hello
+                    </div>
+                    <div>
+                        goodbye
+                    </div>
+                    <div>
+                        hey
+                    </div>
+                    <div>
+                        see ya
+                    </div>
+                    <div>
+                        guten tag
+                    </div>
+                    <div>
+                        tchuess
+                    </div>
+                </Pager>
             </div>
         )
     }
