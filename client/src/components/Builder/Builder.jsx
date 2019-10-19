f5959b8f3e43b3595fa90d3505fca3610158fdb7
Mason Short <ironicallyshort737@hotmail.com>
2019-10-19 12:38:38 -0600

    fixed merge conflicts

--- client/src/components/Builder/Builder.jsx
+++ client/src/components/Builder/Builder.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import '../Character'; 
+import '../Character';
 import "./Builder.css";
 import { Pager } from '../Pagify';
 import API from '../../utils/API';
@@ -7,82 +7,81 @@ import Button from "../Buttons"
 
 class Builder extends Component {
     state = {
-      Name: "",
-      Race: "",
-      Class: "",
-      Stats: "",
-      Background: "",
-      Classes: [],
-      Races: [],
+        Name: "",
+        Race: "",
+        Class: "",
+        Stats: "",
+        Background: "",
+        Classes: [],
+        Races: [],
     }
 
-    submit(event){
-      event.preventDefault();
-      if(this.state.Name.length === 0){
-        console.log("Character needs a name!");
-      }
-      else {
-        this.submitInfo();
-      }
+    submit(event) {
+        event.preventDefault();
+        if (this.state.Name.length === 0) {
+            console.log("Character needs a name!");
+        } else {
+            this.submitInfo();
+        }
     }
 
     componentDidMount() {
-      this.getAllClasses()
-      this.getAllRaces()
+        this.getAllClasses()
+        this.getAllRaces()
     }
 
-// getAllRaces Function 
-    getAllRaces(){
-      API.getAllRaces().then(data => {
-        this.setState({Races: data.data.results})
-      })
+    // getAllRaces Function 
+    getAllRaces() {
+        API.getAllRaces().then(data => {
+            this.setState({ Races: data.data.results })
+        })
     }
-// getAllClasses Function 
-    getAllClasses(){
-      API.getAllClasses().then(data => { 
-        this.setState({Classes: data.data.results}) }
-      )
+    // getAllClasses Function 
+    getAllClasses() {
+        API.getAllClasses().then(data => {
+            this.setState({ Classes: data.data.results })
+        });
     };
-// getAllAbilities Function 
 
+    // getAllAbilities Function 
     handleTextChange = event => {
-      const { name, value } = event.target;
-      this.setState({
-        [name]: value
-      })
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
-      return(
-     <div className="col l6 grey clearfix">
-      <Pager paginate="dots">
-        <div className="characterName">
-          {/* Character Name */}
-          <h1>{this.state.Name}</h1>
-          <input name="Name" value={this.state.Name} onChange={this.handleTextChange} />
-        </div>
-        <div className="characterRaces">
-              {this.state.Races.map(result => <Button title={result.name} />)}
+        return (
+            <div className="col l6 grey clearfix">
+                <Pager paginate="dots">
+                    <div className="characterName">
+                        {/* Character Name */}
+                        <h1>{this.state.Name}</h1>
+                        <input name="Name" value={this.state.Name} onChange={this.handleTextChange} />
+                    </div>
+                    <div className="characterRaces">
+                        {this.state.Races.map(result => <Button title={result.name} />)}
 
-        </div>
-        <div className="characterClasses">
-           {/* Call to getAllClassses */}
-          {this.state.Classes.map(result => <Button title={result.name} />)}
-          
-        </div>
-        <div className="characterAbilities">
-           {/* Call to getAllAbilityScores */}
+                    </div>
+                    <div className="characterClasses">
+                        {/* Call to getAllClassses */}
+                        {this.state.Classes.map(result => <Button title={result.name} />)}
 
-        </div>
-        <div className="characterBackground">
-          {/* Description/Background Field */}
+                    </div>
+                    <div className="characterAbilities">
+                        {/* Call to getAllAbilityScores */}
 
-        </div>
-      </Pager>
-     </div>
-      )
-    } 
-  
+                    </div>
+                    <div className="characterBackground">
+                        {/* Description/Background Field */}
+
+                    </div>
+                </Pager>
+            </div>
+        )
+    }
+
 }
 
 export default Builder;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
-import Builder from "../components/Builder";
-import Character from "../components/Character";
 
 class Home extends Component {
-<<<<<<< HEAD
-  render() {
-    return (
-      <>
-      </>
-    );
-  }
-=======
     render() {
         return (
           <div className="container grey lighten-5">
@@ -21,13 +11,10 @@ class Home extends Component {
                 fontFamily: "Grenze"
               }}
             >
-              <Builder />
-              <Character />
             </div>
           </div>
         );
     }
->>>>>>> b1b0e97795ed6e2d8d922029716a1c995ba40a90
 }
 
 export default Home;
