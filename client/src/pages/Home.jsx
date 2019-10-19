29bd1863a11bbf8d7bc461b05d79f91311003588
Cate Ashley <catemarie.ashley@gmail.com>
2019-10-19 12:23:20 -0600

    trying to fix npm errors

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,18 @@
 import React, { Component } from 'react';
 import '../App.css';
-import Builder from "../components/Builder";
-import Character from "../components/Character";
-
 class Home extends Component {
-<<<<<<< HEAD
   render() {
     return (
-      <>
-      </>
+      <div className="container grey lighten-5">
+        <div
+          className="row"
+          style={{
+            fontFamily: "Grenze"
+          }}
+        >
+        </div>
+      </div>
     );
   }
-=======
-    render() {
-        return (
-          <div className="container grey lighten-5">
-            <div
-              className="row"
-              style={{
-                fontFamily: "Grenze"
-              }}
-            >
-              <Builder />
-              <Character />
-            </div>
-          </div>
-        );
-    }
->>>>>>> b1b0e97795ed6e2d8d922029716a1c995ba40a90
 }
-
 export default Home;
