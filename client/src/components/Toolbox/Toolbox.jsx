97966c27d3d6ba19621723eb912b560bd6d67d20
Mason Short <ironicallyshort737@hotmail.com>
2019-10-25 00:04:57 -0600

    started dice roller embed

--- client/src/components/Toolbox/Toolbox.jsx
+++ client/src/components/Toolbox/Toolbox.jsx
@@ -1,19 +1,16 @@
-<<<<<<< HEAD:client/src/components/Toolbox/Toolbox.jsx
 import React, { Component } from 'react';
-import Whiteboard from 'react-component-whiteboard';
 import "./Toolbox.css";
 
-=======
-// import React, { Component } from 'react';
-// import Whiteboard from 'react-component-whiteboard';
->>>>>>> master:client/src/components/Toolbox.jsx
+class Toolbox extends Component {
+    render() {
+        return (
+            <div id="gameRegion" style=" display: none;">
+                <button id="hideDice">Close Dice Tray</button>
+                <iframe src="./game.html" class="webgl-content" style="border:0px #000000 none;" scrolling="no"
+                    frameborder="1" marginheight="px" marginwidth="960px" height="600" width="960px"></iframe>
+            </div>
+        )
+    }
+}
 
-// class Toolbox extends Component {
-//     render() {
-//         return (
-//             <Whiteboard allowDraw />
-//         ) 
-//     }
-// }
-
-// export default Toolbox;
+export default Toolbox;
