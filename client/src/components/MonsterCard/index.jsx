03fa530a84ab7b781b89764043f752e8c3c55e4b
Spiritbr8ker <stephen.oveson@gmail.com>
2019-10-25 00:52:57 -0600

    Monster card and Spell card update

--- client/src/components/MonsterCard/index.jsx
+++ client/src/components/MonsterCard/index.jsx
@@ -9,19 +9,20 @@ const RaceCard = (props) => {
             <div className="card">
                 <div className="card-content">
                     <h1>{props.title}</h1>
-                    <h6>Average Size: {props.size}</h6>
-                    <p>Senses: {props.senses}</p>
-                    <p>Speed: {props.speed}</p>
-                    <p>Hit Dice: {props.hitDice}</p>
-                    <p>Alignment: {props.alignment}</p>
-                </div>
-                <div className="card-content grey lighten-4">
                     <div>
                         <h6>Type: {props.type}</h6>
                     </div>
                     <div>
                         <h6>Languages: {props.languages}</h6>
                     </div>
+
+                </div>
+                <div className="card-content grey lighten-4">
+                <h6>Average Size: {props.size}</h6>
+                    <p>Senses: {props.senses}</p>
+                    <p>Speed: {props.speed}</p>
+                    <p>Hit Dice: {props.hitDice}</p>
+                    <p>Alignment: {props.alignment}</p>
                     <div>
                         <h6>{props.actions}</h6>
                     </div>
